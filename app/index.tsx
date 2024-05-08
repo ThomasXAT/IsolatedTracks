425de460dc7016073d886bc2fcd1b031a0ca8440
Thomas JORGE <thomas.jorge.xat@gmail.com>
2024-05-08 11:47:10 +0200

    Barre de recherche

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [inputValue, setInputValue] = useState("");
+  const [submittedValue, setSubmittedValue] = useState("");
+
+  const handleSubmit = () => {
+    setSubmittedValue(inputValue);
+  };
+
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        padding: 20
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View
+        style={{
+          flexDirection: "row", 
+        }}
+      >
+        <TextInput 
+          style={{
+            flex: 1,
+            marginRight: 10,
+            height: 35,
+            borderRadius: 2,
+            backgroundColor: "#ffffff",
+            paddingHorizontal: 10,
+          }}
+          onChangeText={(text) => setInputValue(text)}
+        />
+        <Button 
+          title="Search"
+          onPress={handleSubmit}
+        />
+      </View>
+      {submittedValue !== "" && (
+        <View
+          style={{
+            marginTop: 20,
+          }}
+        >
+          <Text>{submittedValue}</Text>
+        </View>
+      )}
     </View>
   );
 }
